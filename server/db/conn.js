e441acb7eafe285b0234687996effa029b1e0509
ankit dhakad <varmaankit0996@gmail.com>
2025-03-04 16:13:43 +0530

    improving

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const DB = `mongodb+srv://ankitdhakad0004:${process.env.MONGO_ATLAS_PW}@cluster0.sm9djs2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(DB)
+
+mongoose.set("debug", true);
 
 mongoose
   .connect(DB, {
-    dbName: 'amazon',
+    dbName: "amazon",
   })
   .then(() => console.log("Database connected"))
   .catch((error) => console.log("Error: " + error.message));
